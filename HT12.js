9b5ad5fe6b20000066dba8521b3211bbdf326a16
Vanyakh <99444827+Vanyakh@users.noreply.github.com>
2023-01-28 23:40:44 +0200

    Add files via upload

---
+++ HT12.js
@@ -0,0 +1,8 @@
+
+document.getElementById("textfield").onfocus = function(){
+	document.getElementById("div").style.display = "block";
+	}
+	
+	document.getElementById("textfield").onblur = function(){
+	document.getElementById("div").style.display = "none";
+	}
