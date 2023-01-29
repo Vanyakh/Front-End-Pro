1273ec9d86a111a85b3f56f90a3c13597a91bd35
Vanyakh <99444827+Vanyakh@users.noreply.github.com>
2023-01-29 15:22:08 +0200

    Delete HT12.js

--- HT12.js
+++
@@ -1,8 +0,0 @@
-
-document.getElementById("textfield").onfocus = function(){
-	document.getElementById("div").style.display = "block";
-	}
-	
-	document.getElementById("textfield").onblur = function(){
-	document.getElementById("div").style.display = "none";
-	}
